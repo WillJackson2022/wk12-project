f739b4f411b08ec96fbc4aee551dc92fce70d6b2
William Jackson <william.jackson09@gmail.com>
2022-09-04 16:33:43 -0400

    Update wk12project.py

--- wk12project.py
+++ wk12project.py
@@ -3,22 +3,25 @@
 
 # The list should be empty initially
 aws_services = []
+print(len(aws_services))
 
 # Populate the list using append or insert
 aws_services.append('EC2')
 aws_services.append('S3')
 aws_services.append('DynamoDB')
 aws_services.append('CodeDeploy')
-aws_services.append('ECS')
 aws_services.append('VPC')
+aws_services.append('ECS')
+
 
 # Print the list and the length of the list
 print(aws_services)
 print(len(aws_services))
 
 # Remove two specific services from the list by name or by index
-del aws_services[4]
-del aws_services[6]
+del aws_services[2]
+del aws_services[3]
+
 print(aws_services)
 
 # Print the new list and the new length of the list
